efe4255ae129716db0dfee66bf2ee1d4db142794
hhamazaki <toshihide.hamazaki@alaska.gov>
2023-10-13 09:34:19 -0800

    magma update

--- magma/create.annual.catch.summary.R
+++ magma/create.annual.catch.summary.R
@@ -89,23 +89,23 @@ create.annual.catch.summary <- function(year, side, wd=wd) {
 
 ################################
  setwd(paste(wd, "/magma", sep=""))
-     magma.data <- readRDS("age_output_stats_dis.Rds") 
-     magma.data <- magma.data$age_summ
+    year <- 2018
+     magma.data <- readRDS(paste0("stock_age_",year,"_stats_dis.Rds")) 
 	 magma <- as.data.frame(do.call(rbind,magma.data))
-	 magma$dist <- c(rep(321,11*9),rep(322,11*9),rep(324,11*9),rep(325,11*9),rep(326,11*9))
+	 magma$dist <- c(rep(321,11*12),rep(322,11*12),rep(324,11*12),rep(325,11*12),rep(326,11*12))
 	 magma$Age <- paste0(substring(magma$age,1,1),'.',substring(magma$age,2,2))
 	 names(magma)[1] <- 'stock'
- west <- create.annual.catch.summary(2022,'west',wd)
- east <- create.annual.catch.summary(2022,'east',wd)
+ west <- create.annual.catch.summary(year,'west',wd)
+ east <- create.annual.catch.summary(year,'east',wd)
  
- RR <- rbind(east$RR,west$RR)
- RR <- melt(RR,id.vars = c('dist','stock'), variable.name='Age',value.name='Prop')
+ RR.s <- rbind(east$RR,west$RR)
+ RR <- melt(RR.s,id.vars = c('dist','stock'), variable.name='Age',value.name='Prop')
  RR.t <- merge(RR,magma[,c('stock','dist','Age','mean')],by=c('dist','stock','Age'),all=TRUE)
  Catch <- data.frame(dist = c(324,322,321,325),Catch = c(east$Catch,west$Catch))
  Stock <- data.frame(dist=c(rep(324,5),rep(322,5),rep(321,5),rep(325,3)),stock=c(rep(c('Kvichak','Alagnak','Naknek','Egegik','Ugashik'),3),'Igushik', 'Wood', 'Nushagak'),ps =  c(t(east$CatchGen),west$CatchGen))
  ob.Catch.age <- data.frame(rbind(east$CatchAge,west$CatchAge))
  ob.Catch.age$dist <- c(324,322,321,325)
- ob.Catch.age.p <- cbind(ob.Catch.age$dist,ob.Catch.age[,2:19]/ob.Catch.age[,20])
+ ob.Catch.age.p <- cbind(ob.Catch.age$dist,ob.Catch.age[,-1]/rowSums(ob.Catch.age[,-1]))
  names(ob.Catch.age.p) <- c('dist',c("0.1","0.2","0.3","0.4","0.5",
                      "1.1","1.2","1.3","1.4","1.5",
                      "2.1","2.2","2.3","2.4",
@@ -117,48 +117,29 @@ create.annual.catch.summary <- function(year, side, wd=wd) {
  RR.t <- merge(RR.t,Catch.age.p,by=c('dist','Age'),all=TRUE)
  RR.t$c.RR  <- with(RR.t, Prop*Catch*ps)
  RR.t$c.magma  <- with(RR.t, mean*Catch*ps)
-
  RR.t <- RR.t[order(RR.t$dist,RR.t$stock,RR.t$Age),]
  head(RR.t)
- 
- dist_stock <- data.frame(dist=c(rep(324,5),rep(322,5),rep(321,5),rep(325,3)),stock= c(rep(c('Kvichak','Alagnak','Naknek','Egegik','Ugashik'),3),'Igushik', 'Wood', 'Nushagak')) 
-windows(record=TRUE)
+ dist_stock <- data.frame(dist=c(rep(324,5),rep(322,5),rep(321,5),rep(325,3)), dist.n=c(rep('Naknek-Kvichak',5),rep('Egegik',5),rep('Ugashik',5),rep('Nushagak',3)),
+ stock= c(rep(c('Kvichak','Alagnak','Naknek','Egegik','Ugashik'),3),'Igushik', 'Wood', 'Nushagak')) 
+#windows(record=TRUE)
 ########  Add Texts  #######################################################
-par(mfrow=c(4,5),mar = c(2,1.5,1.5,2),oma = c(3,3,3,3)) 
+par(mfrow=c(4,5),mar = c(2,2,3,2),oma = c(3,3,3,3)) 
 for(i in 1:18){
- barplot(t(RR.t[which(RR.t$dist==dist_stock[i,1]&RR.t$stock==dist_stock[i,2]),c(5,10)]),col=c(2,3),beside=TRUE,ylim=c(0,1),
+ barplot(t(RR.t[which(RR.t$dist==dist_stock[i,1]&RR.t$stock==dist_stock[i,3]),c('Prop','mean')]),col=c(2,3),beside=TRUE,ylim=c(0,1),
  names.arg = c("0.1","0.2","0.3","0.4","0.5",
                      "1.1","1.2","1.3","1.4","1.5",
                      "2.1","2.2","2.3","2.4",
                      "3.1","3.2","3.3","3.4")
 					 ,
- main = paste(dist_stock[i,1],dist_stock[i,2]),las=3
-   )
+ main = paste(dist_stock[i,1],dist_stock[i,2],'\n',dist_stock[i,3]),las=3
+)   
 }
-legend('topright',legend=c('magma','Catch'), fill= c(2,3),bty='n') 
+legend('topright',legend=c('RR','magma'), fill= c(2,3),bty='n') 
 ########  Add Texts  #######################################################
-mtext("Harvest age prob by dist,stock", side = 3, line = 0, outer = TRUE,las=1)
+mtext(paste("Harvest age prob by dist,stock",year), side = 3, line = 0, outer = TRUE,las=1)
 mtext('Age prop', side = 2, line = 1, outer = TRUE)
 mtext("Age", side = 1, line = 1, outer = TRUE,las=1)
 
-par(mfrow=c(4,5),mar = c(2,1.5,1.5,2),oma = c(3,3,3,3)) 
-for(i in 1:18){
- barplot(t(RR.t[which(RR.t$dist==dist_stock[i,1]&RR.t$stock==dist_stock[i,2]),c(4:5,10)]),col=c(2,3,5),beside=TRUE,ylim=c(0,1),
- names.arg = c("0.1","0.2","0.3","0.4","0.5",
-                     "1.1","1.2","1.3","1.4","1.5",
-                     "2.1","2.2","2.3","2.4",
-                     "3.1","3.2","3.3","3.4")
-					 ,
- main = paste(dist_stock[i,1],dist_stock[i,2]),las=3
-   )
-}
-legend('topright',legend=c('RR','magma','Catch'), fill= c(2,3,5),bty='n') 
-########  Add Texts  #######################################################
-mtext("Harvest age prob by dist,stock", side = 3, line = 0, outer = TRUE,las=1)
-mtext('Age prop', side = 2, line = 1, outer = TRUE)
-mtext("Age", side = 1, line = 1, outer = TRUE,las=1)
-  
-  
 temp.RR <- aggregate(c.RR~stock+Age,FUN=sum, data=RR.t)
 temp.magma <- aggregate(c.magma~stock+Age,FUN=sum, data=RR.t)
 Catch.age <- merge(temp.RR,temp.magma, by = c('stock','Age'),all=TRUE)
@@ -175,10 +156,9 @@ barplot(t(Catch.age[which(Catch.age$stock==stock.t[i]),3:4]),col=c(2,3),beside=T
 }
 legend('topright',legend=c('RR','magma'), fill= c(2,3),bty='n') 
 ########  Add Texts  #######################################################
-mtext("Harvest by stock,Age", side = 3, line = 0, outer = TRUE)
+mtext(paste("Harvest by stock,Age",year), side = 3, line = 0, outer = TRUE)
 mtext('Harvest', side = 2, line = 1, outer = TRUE)
 mtext("Age", side = 1, line = 1, outer = TRUE)
-
 
 temp.RR <- aggregate(c.RR~dist+Age,FUN=sum, data=RR.t)
 temp.magma <- aggregate(c.magma~dist+Age,FUN=sum, data=RR.t)
@@ -186,12 +166,11 @@ Catch.age <- merge(temp.RR,temp.magma, by = c('dist','Age'),all=TRUE)
 ob.Catch.age <- merge(ob.Catch.age, Catch,by=c('dist'),all=TRUE)
 ob.Catch.age[,2:19] <-ob.Catch.age[,2:19]*ob.Catch.age[,20]
   
-
- 
 dist <- c(324,322,321,325)
+dist.name <- c('Naknek-Kvichak','Egegik','Ugashik','Nushagak')
 par(mfrow=c(2,2),mar = c(2,1.5,1.5,2),oma = c(3,3,3,3))
 for(i in 1:4){
-temp1 <- t(Catch.age[which(Catch.age$dist==dist[i]),3:4])
+temp1 <- t(Catch.age[which(Catch.age$dist==dist[i]),c('c.RR','c.magma')])
 temp2 <- ob.Catch.age[which(ob.Catch.age$dist==dist[i]),2:19]
 colnames(temp1) <- names(temp2)
 barplot(as.matrix(rbind(temp1,temp2)),col=c(2,3,4),beside=TRUE,
@@ -199,12 +178,12 @@ barplot(as.matrix(rbind(temp1,temp2)),col=c(2,3,4),beside=TRUE,
                      "1.1","1.2","1.3","1.4","1.5",
                      "2.1","2.2","2.3","2.4",
                      "3.1","3.2","3.3","3.4"),
- main = paste(dist[i]),las=3
+ main = paste(dist[i],dist.name[i]),las=3
    )
 }
 legend('topright',legend=c('RR','magma','obs'), fill= c(2,3,4),bty='n') 
 ########  Add Texts  #######################################################
-mtext("Harvest by Dist,Age", side = 3, line = 0, outer = TRUE)
+mtext(paste("Harvest by Dist,Age",year), side = 3, line = 0, outer = TRUE)
 mtext('Harvest', side = 2, line = 1, outer = TRUE)
 mtext("Age", side = 1, line = 1, outer = TRUE)
 
